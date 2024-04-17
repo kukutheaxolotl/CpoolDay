7b1448d6180875e697ba7901631ece245361ddb5
kukutheaxolotl <156931872+kukutheaxolotl@users.noreply.github.com>
2024-04-17 13:47:13 +0000

    [ADD] new file

--- my_print_revert.c
+++ my_print_revert.c
@@ -1,8 +1,18 @@
 void my_print_revert(char *toRevert);
 void print_char(char c);
+int my_strlen(char *src);
 
 void my_print_revert(char *toRevert){
-    for(int i = 9;i>=0;i--){
+    for(int i =my_strlen(toRevert);i>=0;i--){
         print_char(toRevert[i]);
     }
 }
+int my_strlen(char *src){
+    int j =0;
+    for(int i = 0; src[i] != '\0'; i++){
+        j = i;
+    }
+    return j;
+
+}
+
