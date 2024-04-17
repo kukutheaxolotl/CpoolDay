80eb1770d4b4dfd996c12cf529ef06a6f8b305d1
kukutheaxolotl <156931872+kukutheaxolotl@users.noreply.github.com>
2024-04-17 11:41:46 +0000

    [ADD]my_print_revert

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,5 +1,10 @@
 void print_char(char c);
 void my_print_ascii(void);
+int main(void)
+{
+    my_print_ascii;
+    return 0;
+}
 void my_print_ascii(void)
 {
     for (int i = 33; i < 127 ; i++){
---
+++ my_print_revert.c
@@ -0,0 +1,8 @@
+void my_print_revert(char *toRevert);
+void print_char(char c);
+
+void my_print_revert(char *toRevert){
+    for(int i = 9;i>==0;i--){
+        print_char(toRevert[i]);
+    }
+}
