fc96e255a52ac953c310698bf06e99bae94bb913
kukutheaxolotl <156931872+kukutheaxolotl@users.noreply.github.com>
2024-04-17 13:51:46 +0000

    [ADD]new file

---
+++ my_get_char_repeat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int my_get_char_repeat(char to_find, const char *to_search);
+
+int my_get_char_repeat(char to_find, const char *to_search){
+    int j = 0;
+    for(int i=0;to_search[i]!='\0';i++){
+        if(to_search[i]==to_find){
+            j++;
+        }
+    }
+    return j;
+}
